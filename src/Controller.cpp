2f25ed99d1e42e040118e3d88bd7cff3f7cf6268
Vancior <vancior@icloud.com>
2018-04-24 14:14:22 +0800

    AverageMatchTime in Controller

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -3,28 +3,37 @@
 
 Controller::Controller(Sim &sim, Solution &solution) : sim_(sim), solution_(solution)
 {
+	total_time_ = 0;
+	total_match_ = 0;
 }
 
-Controller::RequestOnline(const trip_t &request)
+void Controller::RequestOnline(const trip_t &request)
 {
 	solution.RequestOnline(request);
 	t_starts_[request.id] = std::chrono::high_resolution_clock::now();
 }
 
-Controller::VehicleOnline(const trip_t &vehicle)
+void Controller::VehicleOnline(const trip_t &vehicle)
 {
 	solution.VehicleOnline(vehicle);
 }
 
-Controller::UpdateRoute(unsigned int veh_id, const std::vector<unsigned int> &route)
+void Controller::UpdateRoute(unsigned int veh_id, const std::vector<unsigned int> &route)
 {
 	sim.UpdateRoute(veh_id, route);
 }
 
-Controller::SetMatch(unsigned int req_id, unsigned int veh_id)
+void Controller::SetMatch(unsigned int req_id, unsigned int veh_id)
 {
 	// call the logger
 	auto t_end = std::chrono::high_resolution_clock::now();
 	int elpased = std::round(std::chrono::duration<double, std::milli>(t_end - t_starts_[req_id]).count());
 	std::cout << "request " << req_id << " matched in " << elpased << " ms" << std::endl;
+	total_time_ += elpased;
+	total_match_++;
 }	
+
+int Controller::AverageMatchTime()
+{
+	return std::round((double)total_time_ / total_match_);
+}
