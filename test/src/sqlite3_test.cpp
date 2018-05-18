6f5f6cf5fea086d49c2e61b0942ff74e954baff8
James Pan <jamesjpan@outlook.com>
2018-05-19 01:20:53 +0800

    add sqlite3 test

--- test/src/sqlite3_test.cpp
+++ test/src/sqlite3_test.cpp
@@ -5,29 +5,31 @@
 
 using namespace cargo;
 
-    static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-      int i;
-      for(i=0; i<argc; i++){
-          printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        }
-      printf("\n");
-      return 0;
-    }
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+  int i;
+  for (i = 0; i < argc; i++) {
+    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+  }
+  printf("\n");
+  return 0;
+}
+
 TEST_CASE("Sqlite3 works", "[sqlite3]") {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
-    rc = sqlite3_open(":memory:", &db);
-    if(rc){
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return(1);
-    }
-    rc = sqlite3_exec(db, "create table foo;", callback, 0, &zErrMsg);
-    if(rc!=SQLITE_OK){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    }
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
+  rc = sqlite3_open(":memory:", &db);
+  if (rc) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
-    return 0;
+  } else {
+    rc = sqlite3_exec(db, "CREATE TABLE t(x INTEGER);", callback, 0, &zErrMsg);
+    rc = sqlite3_exec(db, "INSERT INTO t VALUES(42);", callback, 0, &zErrMsg);
+    rc = sqlite3_exec(db, "SELECT * FROM t;", callback, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    }
+  }
+  sqlite3_close(db);
 }
