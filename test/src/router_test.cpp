4086b7d773b798d2d8b3dbeab919d5ee4472d127
James Pan <jamesjpan@outlook.com>
2018-05-25 10:13:03 +0800

    edit

--- test/src/router_test.cpp
+++ test/src/router_test.cpp
@@ -7,27 +7,24 @@ using msg::Message;
 using msg::MessageType;
 
 TEST_CASE("Router works", "[router]") {
-    Message m(MessageType::DEFAULT);
-    m << "Router Test Begin" << std::endl;
-
     GTree::load("../data/roadnetworks/tiny.gtree");
-    auto g_ = GTree::getG();
-    m << g_.n << " " << g_.m << std::endl;
-    auto gtree_ = GTree::get();
 
-    Stop o {1, 0, StopType::CUSTOMER_ORIGIN, 0, 0};
-    Stop d {1, 6, StopType::CUSTOMER_DEST, 0, 10};
-    Schedulel s {o, d};
+    WHEN("tiny.gtree is loaded") {
+        auto g_ = GTree::getG();
+        REQUIRE(g_.n == 9);
+        REQUIRE(g_.m == 9);
+    }
 
-    Router sr(gtree_);
-    Routel r;
-
-    int c = sr.RouteThrough(s, r);
-    m << "Cost through (0,6) on tiny == 15: " << c << "\nRoute: ";
-
-    for (auto i = r.begin(); i != r.end(); ++i)
-        m << *i << " ";
-    m << std::endl;
-
-    REQUIRE(c == 15);
+    WHEN("shortest path works") {
+        auto gtree_ = GTree::get();
+        Stop o{1, 0, StopType::CUSTOMER_ORIGIN, 0, 0};
+        Stop d{1, 6, StopType::CUSTOMER_DEST, 0, 10};
+        Schedulel s{o, d};
+        Router sr(gtree_);
+        Routel r_;
+        Routel r{0, 1, 2, 5, 6};
+        int c = sr.RouteThrough(s, r_);
+        REQUIRE(c == 15);
+        REQUIRE(r_ == r);
+    }
 }
