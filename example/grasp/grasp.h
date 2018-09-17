d6dd0b7f0b497b7f3224dea09766e5ad66780ee7
James Pan <jamesjpan@outlook.com>
2018-09-17 14:28:25 +0800

    add grasp

---
+++ example/grasp/grasp.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2018 the Cargo authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#include "libcargo.h"
+
+using namespace cargo;
+
+typedef std::tuple<DistInt, MutableVehicleSptr,
+  std::vector<Stop>, std::vector<Wayp>> Assignment;
+
+class Grasp : public RSAlgorithm {
+ public:
+  Grasp();
+
+  /* My overrides */
+  virtual void handle_vehicle(const Vehicle &);
+  virtual void match();
+  virtual void end();
+  virtual void listen(bool skip_assigned = true, bool skip_delayed = true);
+
+ private:
+  Grid grid_;
+
+  std::mt19937 gen;
+  std::uniform_real_distribution<> d;
+
+  /* Workspace variables */
+  std::vector<Stop> sch;
+  std::vector<Wayp> rte;
+  std::vector<MutableVehicleSptr> candidates;
+  tick_t timeout_0;
+  std::unordered_map<CustId, bool> is_matched;
+  std::vector<std::tuple<Customer, MutableVehicle, DistInt>> best_sol;
+  std::unordered_map<VehlId, std::vector<Customer>> commit_cadd;
+  std::unordered_map<VehlId, std::vector<Wayp>> commit_rte;
+  std::unordered_map<VehlId, std::vector<Stop>> commit_sch;
+
+  void reset_workspace();
+};
+
