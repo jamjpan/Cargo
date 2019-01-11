efdb361d956630c3112479a4c414019b6e40ae7d
James Pan <jamesjpan@outlook.com>
2019-01-11 14:31:01 +0800

    remove includes

--- example/grabby/grabby.cpp
+++ example/grabby/grabby.cpp
@@ -18,10 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 #include <algorithm>
-#include <iostream>
-#include <memory>
-#include <tuple>
-#include <vector>
 
 #include "grabby.h"
 #include "libcargo.h"
